5455c19b359c980bd01d78f6807b688dfc375be6
MonoJuega774 <jpusme02@gmail.com>
2025-10-27 07:22:16 -0500

    bisiesto

--- Bisiesto.java
+++ Bisiesto.java
@@ -5,5 +5,12 @@ class Bisiesto {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese año: ");
         int año = scanner.nextInt();
+
+        if (año % 4 == 0 & año % 100 != 0 || año % 400 == 0) {
+            System.out.println("El año: " + año + ", si es bisiesto");
+        }
+        else {
+            System.out.println("El año: " + año + ", no es bisiesto");
+        }
     }
 }
